27e40aad05a0d6ff5444ef3b3719ccd0dc46b4a3
PME4U <greg.halliday@pme4u.com>
2020-12-28 00:03:36 +1000

    implemented access rights for process types

--- src/app/views/process-types/process-types.component.ts
+++ src/app/views/process-types/process-types.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { ModalDirective } from 'ngx-bootstrap/modal';
+
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import { ProcessTypeService } from '../../services/process-type.service';
+import { UserAccessService } from '../../services/user-admin.service';
+
 import { ProcessType, sortAlpha } from '../../models/process-type.model';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-process-types',
@@ -16,11 +19,19 @@ import { map } from 'rxjs/operators';
 export class ProcessTypesComponent implements OnInit {
   tableData$: Observable<ProcessType[]>;
   maintForm: FormGroup;
+  baseUrl: string = 'api/system-parameter/process-type-list/';
+  scope = 'system_params';
+
+  no_access: boolean = true;
+  read_only: boolean = false;
+  modify: boolean = false;
+  create: boolean = false;
+  delete: boolean = false;
+
   recordTitle: string;
   id = null;
   editing: boolean;
   isFetching: boolean = false;
-  baseUrl: string = 'api/system-parameter/process-type-list/';
   totalRecords: number;
   isActive: boolean;
   inProgress: boolean;
@@ -31,32 +42,43 @@ export class ProcessTypesComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private processTypeService: ProcessTypeService
-  ) {
+    private processTypeService: ProcessTypeService,
+    private userAccessService: UserAccessService
+  ) {}
+
+  ngOnInit(): void {
+    this.no_access = this.userAccessService.isNoAccess(this.scope);
+    this.read_only = this.userAccessService.isReadOnly(this.scope);
+    this.modify = this.userAccessService.isModify(this.scope);
+    this.create = this.userAccessService.isCreate(this.scope);
+    this.delete = this.userAccessService.isDelete(this.scope);
+
+    this.getTableData(this.baseUrl);
     this.createForm();
   }
 
-  ngOnInit(): void {
-    this.getTableData(this.baseUrl);
-  }
-
   getTableData(apiUrl: string) {
-    this.isFetching = true;
+    if (!this.no_access) {
+      this.isFetching = true;
 
-    // const processStatus$ = this.processStatusService.getAll(apiUrl)
-    const processTypeService$ = this.processTypeService
-      .getAll(apiUrl)
-      .pipe(map((processTypeService) => processTypeService.sort(sortAlpha)));
+      // const processStatus$ = this.processStatusService.getAll(apiUrl)
+      const processTypeService$ = this.processTypeService
+        .getAll(apiUrl)
+        .pipe(map((processTypeService) => processTypeService.sort(sortAlpha)));
 
-    this.isFetching = false;
-    this.tableData$ = processTypeService$;
+      this.isFetching = false;
+      this.tableData$ = processTypeService$;
+    }
   }
 
   createForm() {
     this.maintForm = this.fb.group({
       process_type: ['', [Validators.required]],
       process_type_description: ['', [Validators.required]],
-      is_active: [true, [Validators.required]],
+      is_active: [
+        { value: true, disabled: !this.modify },
+        [Validators.required],
+      ],
     });
   }
 
@@ -68,37 +90,17 @@ export class ProcessTypesComponent implements OnInit {
   }
 
   editRecord(record) {
-    this.editing = true;
-    // this.isFetching = true;
-    this.id = record.id;
-    this.maintForm.patchValue({
-      process_type: record.process_type,
-      process_type_description: record.process_type_description,
-      is_active: record.is_active,
-    });
-    // this.processTypeService.getOne(record.id).subscribe(
-    //   (response) => {
-    //     this.isFetching = false;
-
-    //     this.id = response.id;
-    //     this.isActive = response.is_active;
-
-    //     this.maintForm.patchValue({
-    //       process_type: response.process_type,
-    //       process_type_description: response.process_type_description,
-    //       is_active: response.is_active,
-    //     });
-    //     // console.log(response);
-    //     // console.log(this.tableData);
-    //     // console.log('Total records:' + this.totalRecords);
-    //     // console.log(this.next);
-    //     // console.log(this.previous);
-    //   },
-    //   (error) => {
-    //     alert(error.message);
-    //   }
-    // );
-    this.maintModal.show();
+    if (!this.no_access) {
+      this.editing = true;
+      // this.isFetching = true;
+      this.id = record.id;
+      this.maintForm.patchValue({
+        process_type: record.process_type,
+        process_type_description: record.process_type_description,
+        is_active: record.is_active,
+      });
+      this.maintModal.show();
+    }
   }
 
   confirmDelete(record) {
